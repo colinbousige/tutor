e0bf8e99f2e711a90a771b2c4b045271bf1f28df
Colin Bousige <colin.bousige@gmail.com>
2021-12-01 12:56:40 +0100

    fix bug

--- R/tuto.R
+++ R/tuto.R
@@ -1,13 +1,13 @@
-#' tuto
+#' tuto()
 #'
 #' Easy syntax to launch a tutorial.
 #'
 #' @param tuto Tutorial name. If NULL, prints the list of all available tutorials.
 #' @return
 #' @export
-tutorial <- function(tuto = NULL) {
-    if(!is.null(tuto)){
-        learnr::run_tutorial(tuto, package = "tutoR")
+tuto <- function(tutoname = NULL) {
+    if(!is.null(tutoname)){
+        learnr::run_tutorial(tutoname, package = "tutoR")
     }else{
         tutolist <- list.files(dir(system.file("tutorials", package = "tutoR"), full.names = TRUE))
         tutolist <- tutolist[grep("Rmd", tutolist)]
